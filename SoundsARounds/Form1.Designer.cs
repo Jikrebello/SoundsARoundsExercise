7ddf5364632f1bd9bb1aad7a580dfb049dfde334
Jikrebello <33652252+Jikrebello@users.noreply.github.com>
2021-05-19 23:47:43 +0200

    Add files via upload

---
+++ SoundsARounds/Form1.Designer.cs
@@ -0,0 +1,378 @@
+﻿
+namespace SoundsARounds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lblreceipts = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.LblTapeBasePrice = new System.Windows.Forms.Label();
+            this.LblRecordBasePrice = new System.Windows.Forms.Label();
+            this.LblCDBasePrice = new System.Windows.Forms.Label();
+            this.LblTapeTotal = new System.Windows.Forms.Label();
+            this.LblRecordTotal = new System.Windows.Forms.Label();
+            this.LblCDTotal = new System.Windows.Forms.Label();
+            this.LblSubtotal = new System.Windows.Forms.Label();
+            this.LblDiscount = new System.Windows.Forms.Label();
+            this.LblTotal = new System.Windows.Forms.Label();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.BtnCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.CheckDiscount = new System.Windows.Forms.CheckBox();
+            this.NumTapeQuantity = new System.Windows.Forms.NumericUpDown();
+            this.NumRecordQuantity = new System.Windows.Forms.NumericUpDown();
+            this.NumCDQuantity = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.NumTapeQuantity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumRecordQuantity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumCDQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Lblreceipts
+            // 
+            this.Lblreceipts.AutoSize = true;
+            this.Lblreceipts.Location = new System.Drawing.Point(176, 9);
+            this.Lblreceipts.Name = "Lblreceipts";
+            this.Lblreceipts.Size = new System.Drawing.Size(124, 17);
+            this.Lblreceipts.TabIndex = 0;
+            this.Lblreceipts.Text = "Sounds-A-Rounds";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tape:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Record:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "CD:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(88, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Quantity";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(222, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Price";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(358, 84);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 17);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Total";
+            // 
+            // LblTapeBasePrice
+            // 
+            this.LblTapeBasePrice.AutoSize = true;
+            this.LblTapeBasePrice.Location = new System.Drawing.Point(222, 150);
+            this.LblTapeBasePrice.Name = "LblTapeBasePrice";
+            this.LblTapeBasePrice.Size = new System.Drawing.Size(50, 17);
+            this.LblTapeBasePrice.TabIndex = 11;
+            this.LblTapeBasePrice.Text = "R 9.99";
+            // 
+            // LblRecordBasePrice
+            // 
+            this.LblRecordBasePrice.AutoSize = true;
+            this.LblRecordBasePrice.Location = new System.Drawing.Point(222, 117);
+            this.LblRecordBasePrice.Name = "LblRecordBasePrice";
+            this.LblRecordBasePrice.Size = new System.Drawing.Size(50, 17);
+            this.LblRecordBasePrice.TabIndex = 12;
+            this.LblRecordBasePrice.Text = "R 2.99";
+            // 
+            // LblCDBasePrice
+            // 
+            this.LblCDBasePrice.AutoSize = true;
+            this.LblCDBasePrice.Location = new System.Drawing.Point(222, 181);
+            this.LblCDBasePrice.Name = "LblCDBasePrice";
+            this.LblCDBasePrice.Size = new System.Drawing.Size(58, 17);
+            this.LblCDBasePrice.TabIndex = 13;
+            this.LblCDBasePrice.Text = "R 15.99";
+            // 
+            // LblTapeTotal
+            // 
+            this.LblTapeTotal.AutoSize = true;
+            this.LblTapeTotal.Location = new System.Drawing.Point(358, 117);
+            this.LblTapeTotal.Name = "LblTapeTotal";
+            this.LblTapeTotal.Size = new System.Drawing.Size(40, 17);
+            this.LblTapeTotal.TabIndex = 14;
+            this.LblTapeTotal.Text = "- - - -";
+            // 
+            // LblRecordTotal
+            // 
+            this.LblRecordTotal.AutoSize = true;
+            this.LblRecordTotal.Location = new System.Drawing.Point(358, 150);
+            this.LblRecordTotal.Name = "LblRecordTotal";
+            this.LblRecordTotal.Size = new System.Drawing.Size(40, 17);
+            this.LblRecordTotal.TabIndex = 15;
+            this.LblRecordTotal.Text = "- - - -";
+            // 
+            // LblCDTotal
+            // 
+            this.LblCDTotal.AutoSize = true;
+            this.LblCDTotal.Location = new System.Drawing.Point(358, 181);
+            this.LblCDTotal.Name = "LblCDTotal";
+            this.LblCDTotal.Size = new System.Drawing.Size(40, 17);
+            this.LblCDTotal.TabIndex = 16;
+            this.LblCDTotal.Text = "- - - -";
+            // 
+            // LblSubtotal
+            // 
+            this.LblSubtotal.AutoSize = true;
+            this.LblSubtotal.Location = new System.Drawing.Point(358, 233);
+            this.LblSubtotal.Name = "LblSubtotal";
+            this.LblSubtotal.Size = new System.Drawing.Size(40, 17);
+            this.LblSubtotal.TabIndex = 17;
+            this.LblSubtotal.Text = "- - - -";
+            // 
+            // LblDiscount
+            // 
+            this.LblDiscount.AutoSize = true;
+            this.LblDiscount.Location = new System.Drawing.Point(358, 267);
+            this.LblDiscount.Name = "LblDiscount";
+            this.LblDiscount.Size = new System.Drawing.Size(40, 17);
+            this.LblDiscount.TabIndex = 18;
+            this.LblDiscount.Text = "- - - -";
+            // 
+            // LblTotal
+            // 
+            this.LblTotal.AutoSize = true;
+            this.LblTotal.Location = new System.Drawing.Point(358, 309);
+            this.LblTotal.Name = "LblTotal";
+            this.LblTotal.Size = new System.Drawing.Size(40, 17);
+            this.LblTotal.TabIndex = 19;
+            this.LblTotal.Text = "- - - -";
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Location = new System.Drawing.Point(349, 379);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(75, 26);
+            this.BtnExit.TabIndex = 23;
+            this.BtnExit.Text = "Exit";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(256, 235);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(64, 17);
+            this.label17.TabIndex = 24;
+            this.label17.Text = "Subtotal:";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(256, 269);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(67, 17);
+            this.label18.TabIndex = 25;
+            this.label18.Text = "Discount:";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(256, 311);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(44, 17);
+            this.label19.TabIndex = 26;
+            this.label19.Text = "Total:";
+            // 
+            // BtnCalculate
+            // 
+            this.BtnCalculate.Location = new System.Drawing.Point(187, 379);
+            this.BtnCalculate.Name = "BtnCalculate";
+            this.BtnCalculate.Size = new System.Drawing.Size(75, 26);
+            this.BtnCalculate.TabIndex = 29;
+            this.BtnCalculate.Text = "Calculate";
+            this.BtnCalculate.UseVisualStyleBackColor = true;
+            this.BtnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Discount:";
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(268, 379);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(75, 26);
+            this.BtnClear.TabIndex = 32;
+            this.BtnClear.Text = "Clear";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // CheckDiscount
+            // 
+            this.CheckDiscount.AutoSize = true;
+            this.CheckDiscount.Location = new System.Drawing.Point(115, 210);
+            this.CheckDiscount.Name = "CheckDiscount";
+            this.CheckDiscount.Size = new System.Drawing.Size(18, 17);
+            this.CheckDiscount.TabIndex = 33;
+            this.CheckDiscount.UseVisualStyleBackColor = true;
+            // 
+            // NumTapeQuantity
+            // 
+            this.NumTapeQuantity.Location = new System.Drawing.Point(91, 112);
+            this.NumTapeQuantity.Name = "NumTapeQuantity";
+            this.NumTapeQuantity.Size = new System.Drawing.Size(58, 22);
+            this.NumTapeQuantity.TabIndex = 34;
+            // 
+            // NumRecordQuantity
+            // 
+            this.NumRecordQuantity.Location = new System.Drawing.Point(91, 145);
+            this.NumRecordQuantity.Name = "NumRecordQuantity";
+            this.NumRecordQuantity.Size = new System.Drawing.Size(58, 22);
+            this.NumRecordQuantity.TabIndex = 35;
+            // 
+            // NumCDQuantity
+            // 
+            this.NumCDQuantity.Location = new System.Drawing.Point(91, 176);
+            this.NumCDQuantity.Name = "NumCDQuantity";
+            this.NumCDQuantity.Size = new System.Drawing.Size(58, 22);
+            this.NumCDQuantity.TabIndex = 36;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 423);
+            this.Controls.Add(this.NumCDQuantity);
+            this.Controls.Add(this.NumRecordQuantity);
+            this.Controls.Add(this.NumTapeQuantity);
+            this.Controls.Add(this.CheckDiscount);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCalculate);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.LblTotal);
+            this.Controls.Add(this.LblDiscount);
+            this.Controls.Add(this.LblSubtotal);
+            this.Controls.Add(this.LblCDTotal);
+            this.Controls.Add(this.LblRecordTotal);
+            this.Controls.Add(this.LblTapeTotal);
+            this.Controls.Add(this.LblCDBasePrice);
+            this.Controls.Add(this.LblRecordBasePrice);
+            this.Controls.Add(this.LblTapeBasePrice);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Lblreceipts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NumTapeQuantity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumRecordQuantity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumCDQuantity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lblreceipts;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label LblTapeBasePrice;
+        private System.Windows.Forms.Label LblRecordBasePrice;
+        private System.Windows.Forms.Label LblCDBasePrice;
+        private System.Windows.Forms.Label LblTapeTotal;
+        private System.Windows.Forms.Label LblRecordTotal;
+        private System.Windows.Forms.Label LblCDTotal;
+        private System.Windows.Forms.Label LblSubtotal;
+        private System.Windows.Forms.Label LblDiscount;
+        private System.Windows.Forms.Label LblTotal;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Button BtnCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.CheckBox CheckDiscount;
+        private System.Windows.Forms.NumericUpDown NumTapeQuantity;
+        private System.Windows.Forms.NumericUpDown NumRecordQuantity;
+        private System.Windows.Forms.NumericUpDown NumCDQuantity;
+    }
+}
+
---
+++ SoundsARounds/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SoundsARounds
+{
+    public partial class Form1 : Form
+    {
+        // Fields
+        private const string CURRENCEY_SYMBOL = "R ";
+        private int quantityTape;
+        private int quantityRecord;
+        private int quantityCD;
+
+        private float tapePrice;
+        private float recordPrice;
+        private float cdPrice;
+
+        private float tapeSubtotal;
+        private float recordSubtotal;
+        private float cdSubtotal;
+
+        private float subTotal;
+        private float subTotalDiscount;
+        private float discountRate;
+
+
+        // Constructor        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Methods
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tapePrice = 9.99f;
+            recordPrice = 2.99f;
+            cdPrice = 15.99f;
+            discountRate = 0.90f;
+            Clear();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            quantityTape = Convert.ToInt32(NumTapeQuantity.Text);
+            quantityRecord = Convert.ToInt32(NumRecordQuantity.Text);
+            quantityCD = Convert.ToInt32(NumCDQuantity.Text);
+
+            tapeSubtotal = quantityTape * tapePrice;
+            recordSubtotal = quantityRecord * recordPrice;
+            cdSubtotal = quantityCD * cdPrice;
+
+            LblTapeTotal.Text = CURRENCEY_SYMBOL + tapeSubtotal.ToString();
+            LblRecordTotal.Text = CURRENCEY_SYMBOL + recordSubtotal.ToString();
+            LblCDTotal.Text = CURRENCEY_SYMBOL + cdSubtotal.ToString();
+
+            subTotal = tapeSubtotal + recordSubtotal + cdSubtotal;
+
+            LblSubtotal.Text = CURRENCEY_SYMBOL + subTotal.ToString();
+
+            if (CheckDiscount.Checked)
+            {
+                subTotalDiscount = subTotal * 0.10f;
+                LblDiscount.Text = CURRENCEY_SYMBOL + Math.Round(subTotalDiscount, 2).ToString();
+
+                subTotal *= discountRate;
+                LblTotal.Text = CURRENCEY_SYMBOL + Math.Round(subTotal, 2).ToString();
+            }
+            else
+            {
+                LblDiscount.Text = "- - - -";
+                LblTotal.Text = CURRENCEY_SYMBOL + Math.Round(subTotal, 2).ToString();
+            }
+
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void Clear()
+        {
+            quantityTape = 0;
+            quantityRecord = 0;
+            quantityCD = 0;
+            tapeSubtotal = 0;
+            recordSubtotal = 0;
+            cdSubtotal = 0;
+            subTotal = 0;
+
+            LblTapeTotal.Text = "- - - -";
+            LblRecordTotal.Text = "- - - -";
+            LblCDTotal.Text = "- - - -";
+            LblDiscount.Text = "- - - -";
+            LblSubtotal.Text = "- - - -";
+            LblTotal.Text = "- - - -";
+
+            if (CheckDiscount.Checked)
+            {
+                CheckDiscount.Checked = false;
+            }
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
